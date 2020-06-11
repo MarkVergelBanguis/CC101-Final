8a105854d3269475d7ac243f4ac1a39b17658add
Marky <MarkVergelBanguis@gmail.com>
2020-06-11 12:06:17 -0700

    first

---
+++ images.js
@@ -0,0 +1,87 @@
+const gallery = document.querySelector('#galleryPictures'),
+      aboutPage = document.querySelector('#about'),
+      aboutButton = document.querySelector('.aboutButton');
+let links = document.querySelectorAll('.cuteness'); 
+
+window.addEventListener('load',function(){
+    document.querySelector('body').classList.add("loaded")  
+  
+    const images = document.querySelector('#images');
+      let output = ``;
+                      for(let i = 0; i < 5; i++) {
+                                      output += `
+                                   <div class="col m5ths s6 box-images"> 
+                                   <img src="/Images/Products/${i}.jpg" itemprop="thumbnail" alt="Image description" class="col m12 s12 shadow p-3 mb-5 bg-secondary rounded my-images"/>
+                                   </div>
+                                      `                       
+      }
+     gallery.innerHTML = output;
+  });
+
+aboutButton.addEventListener('click', showAbout);
+
+function showAbout() {
+aboutPage.style.display = 'block';
+gallery.style.display = 'none';
+}
+
+
+function currentPage(page,length) {
+    let current = document.querySelector('#dropdownMenuLink');
+    current.textContent = page;
+    gallery.style.display = 'flex'
+    about.style.display = 'none';
+    let output = ``;
+                    for(let i = 0; i < length; i++) {
+                        output += `
+                        <figure itemprop="associatedMedia" class="col m5ths s6 box-images">
+                <a href="/Images/${page}/${i}.jpg" itemprop="contentUrl" data-size="800x700"  >
+                    <img src="/Images/${page}/${i}.jpg" itemprop="thumbnail" alt="Image description" class="col m12 s12 shadow p-3 mb-5 bg-secondary rounded my-images"/>
+                </a>
+                <figcaption itemprop="caption description">${i}</figcaption>
+            </figure>`
+                    }
+        return gallery.innerHTML = output;
+}
+
+
+
+
+
+
+
+
+
+
+            links.forEach(link => {
+                            link.addEventListener('click', (e)=> {
+                                const page = e.target.textContent;
+                                if(page ==='Perfume') {                   
+                                    currentPage(page, 25)
+                                } else if (page ==='Jeans') {
+                                    currentPage(page, 10)
+                                } else if (page ==='T-Shirt') {
+                                    currentPage(page ,10)
+                                } else if (page ==='Underwear') {
+                                    currentPage(page, 6)
+                                } else if (page ==='Wax') {
+                                    currentPage(page,6)
+                                } else if (page ==='Categories') {
+                                    currentPage(page,5)
+                                } 
+                        
+                            })
+                    });
+
+
+
+
+
+
+
+     /*    <figure itemprop="associatedMedia" class="col m5ths s6 box-images">
+                <a href="/Images/Perfume/${i}.jpg" itemprop="contentUrl" data-size="800x700"  >
+                    <img src="/Images/Perfume/${i}.jpg" itemprop="thumbnail" alt="Image description" class="col m12 s12 shadow p-3 mb-5 bg-secondary rounded my-images"/>
+                </a>
+                <figcaption itemprop="caption description">${i}</figcaption>
+            </figure> */ 
